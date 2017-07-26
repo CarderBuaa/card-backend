2d531854c84ec6e6400898e68b77963e66f992c8
z <502982165@qq.com>
2017-07-26 12:40:02 +0800

    新增UserService接口和未实现的UserController

---
+++ src/main/java/cn/card/controller/UserController.java
@@ -0,0 +1,43 @@
+package cn.card.controller;
+
+import org.apache.ibatis.annotations.ResultMap;
+import org.junit.runner.Request;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.card.domain.UserQueryVo;
+import cn.card.service.UserService;
+
+/**
+ * 
+ * Description: 控制用户行为的controller
+ * @author z
+ * @date 2017年7月26日
+ */
+@Controller
+public class UserController {
+
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping(value="/user",method=RequestMethod.POST)
+	public void createUserController(@RequestBody UserQueryVo userQueryVo){
+		
+	}
+	
+	@RequestMapping(value="/user/{username}",method= {RequestMethod.GET, RequestMethod.PUT})
+	public void getAndEditUserController(@PathVariable("username") String username, 
+										 @RequestBody UserQueryVo userQueryVo) {
+		
+	}
+	
+	@RequestMapping(value="/user/accesstoken")
+	public void getAccessToken(@RequestBody UserQueryVo userQueryVo) {
+		
+	}
+}
